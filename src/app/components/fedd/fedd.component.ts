267642ce0193df21e5dc0ee5d03c5990af9cd53b
Conrado Zapata <excz.test@gmail.com>
2019-02-22 17:00:41 -0500

    Feed con errores... Corregir

--- src/app/components/fedd/fedd.component.ts
+++ src/app/components/fedd/fedd.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { ChatService } from 'src/app/services/chat.service';
+import { AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { ChatMessage } from 'src/app/models/ChatMessage.model';
 
 @Component({
   selector: 'app-fedd',
   templateUrl: './fedd.component.html',
   styleUrls: ['./fedd.component.css']
 })
-export class FeddComponent implements OnInit {
 
-  constructor() { }
+export class FeddComponent implements OnInit, OnChanges {
+
+  feed: AngularFireList<ChatMessage[]>;
+  constructor(
+    private _chat: ChatService
+  ) { }
 
   ngOnInit() {
+    console.log('feed initial!')
+    this.feed = this._chat.getMessages();
+  }
+  ngOnChanges() {
+    this.feed = this._chat.getMessages();
   }
 
 }
--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ChatService } from 'src/app/services/chat.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { ChatMessage } from 'src/app/models/ChatMessage.model';
 
 @Component({
   selector: 'app-message',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
+  @Input() chatMessage: ChatMessage;
+  userMail:string;
+  username:string;
+  messageContent: string;
+  timestamp: Date = new Date();
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(chatMessage = this.chatMessage) {
+    this.messageContent= chatMessage.message;
+    this.timestamp= chatMessage.timeSent;
+    this.userMail = chatMessage.email;
+    this.username= chatMessage.username;
   }
 
 }
